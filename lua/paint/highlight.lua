6712bf69d9544db1b2200ecb4270528753888267
DragonBoom <1312449403@qq.com>
2024-04-25 12:42:43 +0800

    add comment cn

--- lua/paint/highlight.lua
+++ lua/paint/highlight.lua
@@ -7,6 +7,8 @@ M.enabled = false
 ---@type table<number,number>
 M.bufs = {}
 
+-- 缓存已处理行，之后将跳过这些行
+-- 将通过置为 nil 来清空缓存
 local bufLineBorderCache = {} -- {'buf1' = {1=1, 2=1, 3=0, }, }
 
 ---@param buf number
@@ -27,22 +29,29 @@ function M.highlight(buf, first, last, onlyCursorLine, skipCache)
         bufLineBorderCache[buf] = borderCache
     end
 
+    -- 判断还有哪些行需要处理：重新计算 first、last
     if not skipCache then
         if onlyCursorLine then
+            -- 只需处理光标行
             if buf ~= vim.api.nvim_get_current_buf() then return end
             first = vim.fn.line '.'
             last = first
             vim.api.nvim_buf_clear_namespace(buf, config.ns, first - 1, last) -- only clear here
         else
-            if not (borderCache[first] and borderCache[last] or first == 0 or last == endLine) then
+            -- 尝试扩大处理范围，避免频繁处理：最多扩大1倍
+            if not ((borderCache[first] and borderCache[last]) or first == 0 or last == endLine) and last - first > 5 then
+                -- 其中一个条件：未处理行低于 1/3（数量太少，所以要扩大范围）
+                local threshold = (last - first) / 3
                 local unCacheCount = 0
                 for i = first, last do
                     if not borderCache[i] then
                         unCacheCount = unCacheCount + 1
+                        if unCacheCount >= threshold then
+                            break
+                        end
                     end
                 end
-                -- pre process:
-                if last - first > 10 and unCacheCount < (last - first) / 3 then
+                if unCacheCount < threshold then
                     local newCount = last - first
                     if borderCache[first] then
                         last = last + newCount
@@ -59,10 +68,17 @@ function M.highlight(buf, first, last, onlyCursorLine, skipCache)
     local lines = vim.api.nvim_buf_get_lines(buf, first - 1, last, false)
     local highlights = M.get_highlights(buf)
 
+    local foldclosed = vim.fn.foldclosedend(first)
     for l, line in ipairs(lines) do
         local lnum = first + l - 1
+        -- skip folded
+        if foldclosed == -1 then
+            foldclosed = vim.fn.foldclosedend(lnum)
+        end
+        if lnum < foldclosed then
+            -- 此时位于折叠内，跳过：对有大量折叠行的情况能有较大的提升（否则，由于折叠，屏幕内会出现大量行，全都处理会导致严重卡顿）
 
-        if skipCache or onlyCursorLine or borderCache[lnum] ~= 1 then
+        elseif skipCache or onlyCursorLine or borderCache[lnum] ~= 1 then -- 注意：这里的判断会导致 跳过已缓存的行
             if not skipCache then
                 borderCache[lnum] = 1
             end
@@ -88,7 +104,6 @@ function M.highlight(buf, first, last, onlyCursorLine, skipCache)
             end
         end
     end
-    bufLineBorderCache[buf] = borderCache
 end
 
 ---@return PaintHighlight[]
